f361a3754c67cea4f9949e944902e99c6f4896d1
phamthienan123 <phamthienanlop5c@gmail.com>
2025-07-25 14:46:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -113,7 +113,9 @@ def sell_item(item):
 def admin():
     if 'username' not in session or session['username'] != 'admin':
         return redirect('/')
-    users = {u: data for u, data in USERS.items() if u != 'admin'}
+    with open('users.json','r') as f
+        users=json.load(f)
+    with open('items.json','r') as f
     return render_template('admin.html', users=users, items=ITEMS)
 
 @app.route('/give/<username>', methods=['POST'])
